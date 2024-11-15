c8bd229cb6202e7d129c2aa455cd1f1f548e9ce4
ADMIN <ADMIN@DESKTOP-KE2NFQC>
2024-11-15 13:49:08 +0530

    ok change is done

--- dummy2/src/test/java/github/GitHub.java
+++ dummy2/src/test/java/github/GitHub.java
@@ -1,6 +1,14 @@
 package github;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class GitHub {
+	
+	@Test
+	public void addMsg() {
+		Reporter.log("hlw world");
+	}
 	
 
 }
